91201ff79ee592cdbde76f856cf20863ff67ebce
iva-pa <94801213+iva-pa@users.noreply.github.com>
2022-04-02 17:14:49 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-center">Hello</h1>
+        <h1 className="text-center">Dictionary</h1>
+        <Dictionary />
       </header>
+      <footer className="App-footer">Coded by Iva Papcunova</footer>
     </div>
   );
 }
