29761aa1283d19c61c16d877c9b64ac387fb423f
iva-pa <94801213+iva-pa@users.noreply.github.com>
2022-04-05 20:58:21 +0200

    Added link to github and changed css of App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,17 @@ export default function App() {
         <h1>Dictionary</h1>
         <Dictionary />
       </header>
-      <footer className="App-footer">Coded by Iva Papcunova</footer>
+      <footer className="App-footer">
+        This project was coded by Iva Papcunova and is{" "}
+        <a
+          href="https://github.com/iva-pa/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
