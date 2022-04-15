0a1b8f3431eb33d948bd5c6d44dc482935b2993e
iva-pa <94801213+iva-pa@users.noreply.github.com>
2022-04-15 12:15:32 +0200

    Changed li to span for better styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,11 +4,11 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          return <span key={index}>{synonym}</span>;
         })}
-      </ul>
+      </div>
     );
   } else {
     return null;
