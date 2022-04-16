8a4ac7ae1bcd677be66f89476d559907b43f3937
iva-pa <94801213+iva-pa@users.noreply.github.com>
2022-04-16 21:31:23 +0200

    Changed audio functionality for React audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Audio from "./Audio";
+import ReactAudioPlayer from "react-audio-player";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <div className="row">
-        <div className="col-1">
-          <Audio audio={props.phonetic.audio} />
+        <div className="col">
+          <ReactAudioPlayer src={props.phonetic.audio} controls />
         </div>
         <div className=" col text">{props.phonetic.text}</div>
       </div>
